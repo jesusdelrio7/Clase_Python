bc2cc0d1efb07cf66d12c834066f413a1b4f2b9e
jesusdelrio7 <jesusdlrio7@gmail.com>
2019-06-18 16:44:15 -0500

    Add files via upload

---
+++ nuevo.py
@@ -0,0 +1,13 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print
+<built-in function print>
+>>> print ("Hello World")
+Hello World
+>>> a=12
+>>> a
+12
+>>> for a=12
+SyntaxError: invalid syntax
+>>> 
